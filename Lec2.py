0db68e76c18da1b109d3eddb32b71a8263309357
EC2 Default User <ec2-user@ip-172-31-26-2.ec2.internal>
2023-01-26 21:09:18 +0000

    lec 2

---
+++ Lec2.py
@@ -0,0 +1,22 @@
+'''
+lec 2
+numbers, strings, and variables
+'''
+#print (  type('hello')  )
+#print (  type(123)  )
+#print (  type(123.123)  )
+print('hello')
+print( "its our 2nd Python class")
+print ('hello world'.upper())
+print ('  hello world               '.strip())
+print ('hello world')
+print ('hello world'.split()) #default, split by spaces
+print ('hello world'.split('o'))
+print ('hello' + 'world')
+print (1+2)
+print (9**3)
+my_num = 123
+print (my_num)
+
+my_num = 234
+print(my_num)
