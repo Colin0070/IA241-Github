773d10271f7ee5b29b1b5df9fba231efc3461256
EC2 Default User <ec2-user@ip-172-31-26-2.ec2.internal>
2023-03-02 21:53:16 +0000

    lab7.py

---
+++ Lec7.py
@@ -0,0 +1,65 @@
+'''
+lec7
+while loop
+continue, break, and pass
+'''
+'''
+i=5
+while i>0:
+    print(i)
+
+i=5
+while i>0:
+    i=i-1
+    print(i)
+   # i=i-1
+'''
+'''
+i=5
+while i>0:
+    i=i-1
+    print(i)
+    
+    if i ==3:
+        break
+'''  
+'''
+for item in ['a','b']:
+    print(item)
+    i=5
+    while i>0:
+        i=i-1
+        print(i)
+        
+        if i ==3:
+            break
+'''
+i=5
+while i>0:
+    i=i-1
+
+    if i ==3:
+        continue
+    print(i)
+         
+i=5
+while i>0:
+    i=i-1
+
+    if i ==3:
+        pass
+    print(i)
+    
+try:
+    print(1/0)
+    
+except:
+    print('handle error2')
+    
+i=5
+while i>0:
+    i=i-1
+    try:
+        print(1/(i-3))
+    except:
+        continue
